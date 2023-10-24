789fe78c1cf26e08fb21b31d1839c4b6009d3239
DeniCosta <denisecosta.codes@gmail.com>
2023-10-23 21:01:28 -0300

    feat: Adiciona rota de login da entidade adestrador

--- src/controllers/AdestradorController.js
+++ src/controllers/AdestradorController.js
@@ -1,100 +1,128 @@
-import AdestradorRepository from "../repository/AdestradorRepository.js"
-import AdestradorModel from "../models/AdestradorModel.js"
-import ValidacaoServices from "../services/AdestradorValidacao.js"
-
+import AdestradorRepository from "../repository/AdestradorRepository.js";
+import AdestradorModel from "../models/AdestradorModel.js";
+import ValidacaoServices from "../services/AdestradorValidacao.js";
 
 class AdestradorController {
+  /**
+   * Método para centralização de rotas no controller
+   * @param {Express} app 
+   */
+  static rotas(app) {
     /**
-     * Método para centralização de rotas no controller
-     * @param {Express} app 
+     * Rota para buscar todos os Adestradores
      */
-    static rotas(app) {
-        /**
-         * Rota para buscar todos os Adestradores
-         */
-        app.get("/adestrador", async (req, res) => {
-            const adestrador = await AdestradorRepository.buscarTodosEmAdestrador()
-            res.status(200).json(adestrador)
-        })
+    app.get("/config", async (req, res) => {
+      const adestrador = await AdestradorRepository.buscarTodosEmAdestrador();
+      res.status(200).json(adestrador);
+    });
 
-        /**
-         * Rota para buscar adestradores pelo id
-         */
-        app.get("/adestrador/:id", async (req, res) => {
-            const id = req.params.id;
-            try {
-                const adestrador = await AdestradorRepository.buscarAdestradorPorId(id);
-                if (adestrador) {
-                    res.status(200).json(adestrador);
-                } else {
-                    res.status(404).json({ error: true, message: `Adestrador não encontrado para o id ${id}` });
-                }
-            } catch (error) {
-                res.status(500).json({ error: true, message: "Erro ao buscar adestrador" });
-            }
-        })
+    /**
+     * Rota para buscar adestradores pelo id
+     */
+    app.get("/config/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const adestrador = await AdestradorRepository.buscarAdestradorPorId(id);
+        if (adestrador) {
+          res.status(200).json(adestrador);
+        } else {
+          res.status(404).json({ error: true, message: `Adestrador não encontrado para o id ${id}` });
+        }
+      } catch (error) {
+        res.status(500).json({ error: true, message: "Erro ao buscar adestrador" });
+      }
+    });
 
-        /**
-         * Rota para deletar adestrador
-         */
-        app.delete("/adestrador/:id", async (req, res) => {
-            const id = req.params.id
-            try {
-                const adestrador = await AdestradorRepository.buscarAdestradorPorId(id);
-                if (adestrador) {
-                    await AdestradorDAO.deletarAdestradorPorId(id);
-                    res.status(200).json({ error: false });
-                } else {
-                    res.status(404).json({ error: true, message: `Adestrador não encontrado para o id ${id}` });
-                }
-            } catch (error) {
-                res.status(500).json({ error: true, message: "Erro ao deletar adestrador" });
-            }
-        })
+    /**
+     * Rota para deletar adestrador
+     */
+    app.delete("/config/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const adestrador = await AdestradorRepository.buscarAdestradorPorId(id);
+        if (adestrador) {
+          await AdestradorRepository.deletarAdestradorPorId(id);
+          res.status(200).json({ error: false });
+        } else {
+          res.status(404).json({ error: true, message: `Adestrador não encontrado para o id ${id}` });
+        }
+      } catch (error) {
+        res.status(500).json({ error: true, message: "Erro ao deletar adestrador" });
+      }
+    });
 
-        // Rota para inserir um novo adestrador
-        app.post("/adestrador", async (req, res) => {
-            const body = req.body;
+    // Rota para inserir um novo adestrador
+    app.post("/config", async (req, res) => {
+      const body = req.body;
 
-            // Verifique se todos os campos obrigatórios estão presentes na requisição
-            if (!body.nome || !body.formacao || body.id_agendamento === undefined || body.id_endereco === undefined) {
-                res.status(400).json({ error: true, message: "Campos obrigatórios não preenchidos" });
-                return; // Encerre a função aqui para evitar a execução do código abaixo
-            }
+      // Verifique se todos os campos obrigatórios estão presentes na requisição
+      if (!body.nome || !body.email || !body.senha) {
+        res.status(400).json({ error: true, message: "Campos obrigatórios não preenchidos" });
+        return; // Encerre a função aqui para evitar a execução do código abaixo
+      }
 
-            const adestradorModelado = new AdestradorModel(body.nome, body.formacao, body.id_agendamento, body.id_endereco);
-            try {
-                await AdestradorRepository.inserirAdestrador(adestradorModelado);
-                res.status(201).json({
-                    error: false,
-                    message: "Cliente criado com sucesso"
-                });
-            } catch (error) {
-                res.status(503).json({ error: true, message: "Servidor indisponível no momento" });
-            }
+      const adestradorModelado = new AdestradorModel(body.nome, body.email, body.senha);
+      try {
+        await AdestradorRepository.inserirAdestrador(adestradorModelado);
+        res.status(201).json({
+          error: false,
+          message: "Adestrador cadastrado com sucesso"
         });
+      } catch (error) {
+        res.status(503).json({ error: true, message: "Servidor indisponível no momento" });
+      }
+    });
 
-        /**
-         * Rota para atualizar um registro de um adestrador
-         */
-        app.put("/adestrador/:id", async (req, res) => {
-            const id = req.params.id
-            const body = req.body
-            try {
-                ValidacaoServices.validaCamposAdestrador(body.nome, body.formacao, body.id_agendamento, body.id_endereco)
-                await ValidacaoServices.validarExistencia(id)
-                const adestradorModelado = new AdestradorModel(body.nome, body.formacao, body.id_agendamento, body.id_endereco)
-                await AdestradorDAO.atualizarAdestradorPorId(id, adestradorModelado)
-                res.status(204).json()
-            } catch (error) {
-                if (error.message == "Campos invalidos") {
-                    res.status(400).json({ error: error.message })
-                } else {
-                    res.status(404).json({ id: id, ...error })
-                }
-            }
-        })
-    }
+    /**
+     * Rota para atualizar um registro de um adestrador
+     */
+    app.put("/config/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      try {
+        ValidacaoServices.validaCamposAdestrador(body.nome, body.email, body.senha);
+        await ValidacaoServices.validarExistencia(id);
+        const adestradorModelado = new AdestradorModel(body.nome, body.email, body.senha);
+        await AdestradorRepository.atualizarAdestradorPorId(id, adestradorModelado);
+        res.status(204).json();
+      } catch (error) {
+        if (error.message === "Campos inválidos") {
+          res.status(400).json({ error: error.message });
+        } else {
+          res.status(404).json({ id: id, ...error });
+        }
+      }
+    });
+
+    /**
+     * Rota para realizar o login de um adestrador
+     */
+    app.post("/login", async (req, res) => {
+      const { email, senha } = req.body;
+
+      // Verifique se o email e senha estão presentes na requisição
+      if (!email || !senha) {
+        res.status(400).json({ error: true, message: "Email e senha são obrigatórios." });
+        return;
+      }
+
+      try {
+        // Realize a autenticação do adestrador com base no email e senha
+        const adestrador = await AdestradorRepository.autenticarAdestrador(email, senha);
+
+        if (adestrador) {
+          // Autenticação bem-sucedida, gere um token JWT e envie como resposta
+          const token = adestrador.generateAuthToken();
+          res.status(200).json({ token });
+        } else {
+          // Autenticação falhou
+          res.status(401).json({ error: true, message: "Credenciais inválidas" });
+        }
+      } catch (error) {
+        res.status(500).json({ error: true, message: "Erro ao realizar o login" });
+      }
+    });
+  }
 }
 
-export default AdestradorController
+export default AdestradorController;
--- src/models/AdestradorModel.js
+++ src/models/AdestradorModel.js
@@ -1,12 +1,31 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import AdestradorValidacao from './AdestradorValidacao';
 
-const Adestrador = mongoose.model("Adestrador", {
-    nome: String,
-    formacao: String,
-    id_agendamento: String,
-    id_endereco: String,
-    email: String,
-    senha: String
-})
+const adestradorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
 
-export default Adestrador;
+adestradorSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+    expiresIn: '7d',
+  });
+  return token;
+};
+
+adestradorSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    // Verificar se a senha atende às validações de AdestradorValidacao
+    AdestradorValidacao.validaSenha(this.password);
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+const Adestrador = mongoose.model('Adestrador', adestradorSchema);
+
+module.exports = Adestrador;
