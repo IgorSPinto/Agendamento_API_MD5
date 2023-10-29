34e9c2e8e3bf973b14ba55673ccff9c2ad1b2133
Denise Costa <denisecosta.codes@gmail.com>
2023-10-29 14:12:26 -0300

    feat: adiciona a coparação de senhas para a utilização do bcrypt

--- src/services/AdestradorValidacao.js
+++ src/services/AdestradorValidacao.js
@@ -1,15 +1,28 @@
 import ValidacaoServices from "./ValidacaoServices.js";
-import Adestrador from "../models/AdestradorModel.js"
+import Adestrador from "../models/AdestradorModel.js";
+import bcrypt from "bcrypt";
 
 class AdestradorValidacao extends ValidacaoServices {
   static async validarBusca(id) {
-    const response = await this.exists(Adestrador, id)
-    return response
+    const response = await this.exists(Adestrador, id);
+    return response;
   }
 
   static validarCamposAdestrador(nome, email, senha) {
     return this.validarNome(nome) && this.validarEmail(email) && this.validarSenha(senha);
   }
+
+  static async compararSenhas(senhaInformada, senhaHash) {
+    return bcrypt.compare(senhaInformada, senhaHash);
+  }
 }
 
 export default AdestradorValidacao;
+
+
+
+
+
+
+
+
